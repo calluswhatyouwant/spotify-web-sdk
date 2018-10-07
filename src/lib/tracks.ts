a881ecbbb81f286f42bebbc2c2f2a176d8c6e99d
JRobsonJr <jrobsonjr16@gmail.com>
2018-10-07 09:00:08 -0300

    Using models in track-related requests

--- src/lib/tracks.ts
+++ src/lib/tracks.ts
@@ -1,14 +1,15 @@
 import { getAxiosSpotifyInstance } from './driver';
+import Track from '../models/track/track';
 
 export const getSeveralTracks = async (ids: Array<number | string>) => {
     const params = { params: { ids } };
     const response = await getAxiosSpotifyInstance()
         .get('/tracks', params);
-    return response.data.tracks;
+    return response.data.tracks.map((trackJson: any) => new Track(trackJson));
 };
 
 export const getTrack = async (id: number | string) => {
     const response = await getAxiosSpotifyInstance()
         .get(`/tracks/${id}`);
-    return response.data;
+    return new Track(response.data);
 };
