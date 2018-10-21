5e68db846bf600badd7b8312b8500e777b2f4e87
José Renan <joserenansl99@gmail.com>
2018-10-21 20:19:05 -0300

    Format all the project with prettier

--- src/start.ts
+++ src/start.ts
@@ -2,8 +2,9 @@ import SpotifyWebApiSdk from './lib';
 
 const main = async () => {
     SpotifyWebApiSdk.driver.login('YOUR TOKEN HERE');
-    const track = await SpotifyWebApiSdk.tracks
-        .getTrack('11dFghVXANMlKmJXsNCbNl');
+    const track = await SpotifyWebApiSdk.tracks.getTrack(
+        '11dFghVXANMlKmJXsNCbNl'
+    );
     console.log(track);
 };
 
