ab805c6f87ad080e1d1fefe6368a1b6b873e71cb
Mariana Dela-Bianca <mariana.bianca@ccc.ufcg.edu.br>
2018-10-31 15:55:51 -0300

    Fix playlist request

--- src/lib/playlists.ts
+++ src/lib/playlists.ts
@@ -5,18 +5,18 @@ import PlaylistTrack from './models/playlist/playlist-track';
 export const getPlaylist = async (id: number | string) => {
     const response = await getAxiosSpotifyInstance().get(`/plylists/${id}`);
     return new Playlist(response.data);
-}
+};
 
 export const getPlaylistTracks = async (id: number | string) => {
     const response = await getAxiosSpotifyInstance().get(
         `/playlists/${id}/tracks`
     );
     return response.items.map(trackJson => new PlaylistTrack(trackJson));
-}
+};
 
 export const getUserPlaylists = async (id: number | string) => {
     const response = await getAxiosSpotifyInstance().get(
-        `/users//${id}/playlists`
+        `/users/${id}/playlists`
     );
     return response.items.map(playlistJson => new Playlist(playlistJson));
-}
+};
