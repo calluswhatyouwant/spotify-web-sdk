4f3f36387dd70ee12d09560f303d3f55ba1c5354
JRobsonJr <jrobsonjr16@gmail.com>
2018-11-11 18:31:17 -0300

    Extract matching attributes functions and add implementation for artist

--- test/album.test.ts
+++ test/album.test.ts
@@ -4,6 +4,7 @@ import nock from 'nock';
 import { albumMock, AlbumMock } from './mocks/album.response';
 import { severalAlbumsMock } from './mocks/several-albums.response';
 import { albumTracksMock } from './mocks/album-tracks.response';
+import { checkMatchingAlbumAttributes } from './common/matching-attributes.test';
 
 import { init, getAlbum, getSeveralAlbums, getAlbumTracks } from '../src/lib';
 import Album from '../src/lib/models/album/album';
@@ -12,30 +13,6 @@ describe('Album requests', () => {
     beforeEach(() => {
         init('SomeToken');
     });
-
-    const checkMatchingAlbumAttributes = (response: Album, mock: AlbumMock) => {
-        expect(response.albumType).to.be.equal(mock.album_type);
-        expect(response.artists).to.have.lengthOf(mock.artists.length);
-        expect(response.availableMarkets).to.be.eql(mock.available_markets);
-        expect(response.copyrights).to.be.eql(mock.copyrights);
-        expect(response.externalIds).to.be.eql(mock.external_ids);
-        expect(response.externalUrls).to.be.eql(mock.external_urls);
-        expect(response.genres).to.be.eql(mock.genres);
-        expect(response.href).to.be.equal(mock.href);
-        expect(response.id).to.be.equal(mock.id);
-        expect(response.images).to.be.eql(mock.images);
-        expect(response.label).to.be.equal(mock.label);
-        expect(response.name).to.be.equal(mock.name);
-        expect(response.popularity).to.be.equal(mock.popularity);
-        expect(response.releaseDate).to.be.equal(mock.release_date);
-        expect(response.releaseDatePrecision).to.be.equal(
-            mock.release_date_precision
-        );
-        expect(response.totalTracks).to.be.equal(mock.total_tracks);
-        expect(response.tracks.total).to.be.equal(mock.tracks.total);
-        expect(response.type).to.be.equal(mock.type);
-        expect(response.uri).to.be.equal(mock.uri);
-    };
 
     describe('#getAlbum()', () => {
         beforeEach(() => {
---
+++ test/common/matching-attributes.test.ts
@@ -0,0 +1,49 @@
+import { expect } from 'chai';
+
+import Album from '../../src/lib/models/album/album';
+import Artist from '../../src/lib/models/artist/artist';
+import { AlbumMock } from '../mocks/album.response';
+import { ArtistMock } from './../mocks/artist.response';
+
+export const checkMatchingAlbumAttributes = (
+    response: Album,
+    mock: AlbumMock
+) => {
+    expect(response.albumType).to.be.equal(mock.album_type);
+    expect(response.artists).to.have.lengthOf(mock.artists.length);
+    expect(response.availableMarkets).to.be.eql(mock.available_markets);
+    expect(response.copyrights).to.be.eql(mock.copyrights);
+    expect(response.externalIds).to.be.eql(mock.external_ids);
+    expect(response.externalUrls).to.be.eql(mock.external_urls);
+    expect(response.genres).to.be.eql(mock.genres);
+    expect(response.href).to.be.equal(mock.href);
+    expect(response.id).to.be.equal(mock.id);
+    expect(response.images).to.be.eql(mock.images);
+    expect(response.label).to.be.equal(mock.label);
+    expect(response.name).to.be.equal(mock.name);
+    expect(response.popularity).to.be.equal(mock.popularity);
+    expect(response.releaseDate).to.be.equal(mock.release_date);
+    expect(response.releaseDatePrecision).to.be.equal(
+        mock.release_date_precision
+    );
+    expect(response.totalTracks).to.be.equal(mock.total_tracks);
+    expect(response.tracks.total).to.be.equal(mock.tracks.total);
+    expect(response.type).to.be.equal(mock.type);
+    expect(response.uri).to.be.equal(mock.uri);
+};
+
+export const checkMatchingArtistAttributes = (
+    response: Artist,
+    mock: ArtistMock
+) => {
+    expect(response.externalUrls).to.be.equal(mock.external_urls);
+    expect(response.followers).to.be.equal(mock.followers);
+    expect(response.genres).to.be.equal(mock.genres);
+    expect(response.href).to.be.equal(mock.href);
+    expect(response.id).to.be.equal(mock.id);
+    expect(response.images).to.be.equal(mock.images);
+    expect(response.name).to.be.equal(mock.name);
+    expect(response.popularity).to.be.equal(mock.popularity);
+    expect(response.type).to.be.equal(mock.type);
+    expect(response.uri).to.be.equal(mock.uri);
+};
