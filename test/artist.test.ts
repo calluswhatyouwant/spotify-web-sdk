311685191e0e6e08d712c7145f223617c36c9967
JRobsonJr <jrobsonjr16@gmail.com>
2018-11-11 23:17:52 -0300

    Implement getSeveralArtists test

--- test/artist.test.ts
+++ test/artist.test.ts
@@ -1,9 +1,11 @@
 import nock from 'nock';
 
 import { artistMock } from './mocks/artist.response';
+import { severalArtistsMock } from './mocks/several-artists.response';
 import { checkMatchingArtistAttributes } from './common/matching-attributes.test';
 
 import { init, getArtist } from '../src/lib';
+import { getSeveralArtists } from './../src/lib/artists';
 
 describe('Artist requests', () => {
     beforeEach(() => {
@@ -20,6 +22,30 @@ describe('Artist requests', () => {
         it('response should match all artist attributes', async () => {
             const artistResponse = await getArtist('1WgXqy2Dd70QQOU7Ay074N');
             checkMatchingArtistAttributes(artistResponse, artistMock);
+        });
+    });
+
+    describe('#getSeveralArtists()', () => {
+        beforeEach(() => {
+            nock('https://api.spotify.com/v1')
+                .get('/artists')
+                .query({
+                    ids: ['5WUlDfRSoLAfcVSX1WnrxN', '1WgXqy2Dd70QQOU7Ay074N'],
+                })
+                .reply(200, severalArtistsMock);
+        });
+
+        it('response should match all artists attributes', async () => {
+            const severalArtistsResponse = await getSeveralArtists([
+                '5WUlDfRSoLAfcVSX1WnrxN',
+                '1WgXqy2Dd70QQOU7Ay074N',
+            ]);
+
+            for (let i = 0; i < severalArtistsResponse.length; i++) {
+                const artistResponse = severalArtistsResponse[i];
+                const artistMock = severalArtistsMock.artists[i];
+                checkMatchingArtistAttributes(artistResponse, artistMock);
+            }
         });
     });
 });
