d267b9fb1d4ac93bf1ce35b69781a20222d85b4c
JRobsonJr <jrobsonjr16@gmail.com>
2018-11-19 21:57:32 -0300

    Implement request for current user's currently playing object

--- src/lib/player.ts
+++ src/lib/player.ts
@@ -2,6 +2,7 @@ import { getAxiosSpotifyInstance } from './driver';
 
 import PlayHistory from './models/player/play-history';
 import CursorBasedPage from './models/paging/cursor-based-page';
+import CurrentlyPlaying from './models/player/currently-playing';
 
 export const getCurrentUserRecentlyPlayedTracks = async (params?: {
     limit?: number;
@@ -11,8 +12,18 @@ export const getCurrentUserRecentlyPlayedTracks = async (params?: {
     if (params && params.before && params.after)
         throw new Error("Only one of 'before' or 'after' should be specified");
     const response = await getAxiosSpotifyInstance().get(
-        `/me/player/recently-played`,
+        '/me/player/recently-played',
         { params }
     );
     return new CursorBasedPage<PlayHistory>(response.data, PlayHistory);
+};
+
+export const getCurrentUserCurrentlyPlayingTrack = async (params?: {
+    market?: string;
+}) => {
+    const response = await getAxiosSpotifyInstance().get(
+        '/me/player/currently-playing',
+        { params }
+    );
+    return new CurrentlyPlaying(response.data);
 };
