4f55ed67ee0eb1516e8fedd117a2ce17cd7f27f0
JRobsonJr <jrobsonjr16@gmail.com>
2018-11-20 00:45:37 -0300

    Implement missing track-related requests

--- src/lib/tracks.ts
+++ src/lib/tracks.ts
@@ -1,5 +1,8 @@
 import { getAxiosSpotifyInstance } from './driver';
+
 import Track from './models/track/track';
+import AudioAnalysis from './models/track/audio-analysis';
+import AudioFeatures from './models/track/audio-features';
 
 export const getSeveralTracks = async (ids: string[]) => {
     if (ids.length > 50) {
@@ -9,12 +12,36 @@ export const getSeveralTracks = async (ids: string[]) => {
             `The maximum number of tracks is 50. See ${exceptionLink} for details`
         );
     }
-    const params = { params: { ids } };
-    const response = await getAxiosSpotifyInstance().get('/tracks', params);
+    const params = { ids: ids.join(',') };
+    const response = await getAxiosSpotifyInstance().get('/tracks', { params });
     return response.data.tracks.map((trackJson: any) => new Track(trackJson));
 };
 
 export const getTrack = async (id: string) => {
     const response = await getAxiosSpotifyInstance().get(`/tracks/${id}`);
     return new Track(response.data);
+};
+
+export const getAudioAnalysisForTrack = async (id: string) => {
+    const response = await getAxiosSpotifyInstance().get(
+        `/audio-analysis/${id}`
+    );
+    return new AudioAnalysis(response.data);
+};
+
+export const getAudioFeaturesForTrack = async (id: string) => {
+    const response = await getAxiosSpotifyInstance().get(
+        `/audio-features/${id}`
+    );
+    return new AudioFeatures(response.data);
+};
+
+export const getAudioFeaturesForSeveralTracks = async (ids: string[]) => {
+    const params = { ids: ids.join(',') };
+    const response = await getAxiosSpotifyInstance().get(`/audio-features`, {
+        params,
+    });
+    return response.data.audio_features.map(
+        (audioFeaturesJson: any) => new AudioFeatures(audioFeaturesJson)
+    );
 };
