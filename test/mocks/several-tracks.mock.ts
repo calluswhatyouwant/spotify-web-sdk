27e7a05d1fe36da97d4143b2dada7cb7e810166d
JRobsonJr <jrobsonjr16@gmail.com>
2018-11-20 20:35:57 -0300

    Implement tests for getTrack and getSeveralTracks

---
+++ test/mocks/several-tracks.mock.ts
@@ -0,0 +1,178 @@
+export const severalTracksMock = {
+    tracks: [
+        {
+            album: {
+                album_type: 'album',
+                artists: [
+                    {
+                        external_urls: {
+                            spotify:
+                                'https://open.spotify.com/artist/1WgXqy2Dd70QQOU7Ay074N',
+                        },
+                        href:
+                            'https://api.spotify.com/v1/artists/1WgXqy2Dd70QQOU7Ay074N',
+                        id: '1WgXqy2Dd70QQOU7Ay074N',
+                        name: 'AURORA',
+                        type: 'artist',
+                        uri: 'spotify:artist:1WgXqy2Dd70QQOU7Ay074N',
+                    },
+                ],
+                external_urls: {
+                    spotify:
+                        'https://open.spotify.com/album/2iv4eCuGKJYsso1mDR48dt',
+                },
+                href:
+                    'https://api.spotify.com/v1/albums/2iv4eCuGKJYsso1mDR48dt',
+                id: '2iv4eCuGKJYsso1mDR48dt',
+                images: [
+                    {
+                        height: 640,
+                        url:
+                            'https://i.scdn.co/image/53a31a48a6db327175be725301ba758bb3bed354',
+                        width: 640,
+                    },
+                    {
+                        height: 300,
+                        url:
+                            'https://i.scdn.co/image/0c4210e5b5a44ed2dc6bb42260a4b97c0b0b46a9',
+                        width: 300,
+                    },
+                    {
+                        height: 64,
+                        url:
+                            'https://i.scdn.co/image/0e874917c51fe5ab7413f364b1ef1054920b9372',
+                        width: 64,
+                    },
+                ],
+                name: 'Infections Of A Different Kind (Step I)',
+                release_date: '2018-09-28',
+                release_date_precision: 'day',
+                total_tracks: 8,
+                type: 'album',
+                uri: 'spotify:album:2iv4eCuGKJYsso1mDR48dt',
+            },
+            artists: [
+                {
+                    external_urls: {
+                        spotify:
+                            'https://open.spotify.com/artist/1WgXqy2Dd70QQOU7Ay074N',
+                    },
+                    href:
+                        'https://api.spotify.com/v1/artists/1WgXqy2Dd70QQOU7Ay074N',
+                    id: '1WgXqy2Dd70QQOU7Ay074N',
+                    name: 'AURORA',
+                    type: 'artist',
+                    uri: 'spotify:artist:1WgXqy2Dd70QQOU7Ay074N',
+                },
+            ],
+            disc_number: 1,
+            duration_ms: 239533,
+            explicit: false,
+            external_ids: {
+                isrc: 'GBUM71801202',
+            },
+            external_urls: {
+                spotify:
+                    'https://open.spotify.com/track/6HmBWGDgxHNlE3AvX5aNN5',
+            },
+            href: 'https://api.spotify.com/v1/tracks/6HmBWGDgxHNlE3AvX5aNN5',
+            id: '6HmBWGDgxHNlE3AvX5aNN5',
+            is_local: false,
+            is_playable: true,
+            name: 'Soft Universe',
+            popularity: 49,
+            preview_url:
+                'https://p.scdn.co/mp3-preview/b0ffc1be502212bf4832c10f063b5986ec1b11f7?cid=774b29d4f13844c495f206cafdad9c86',
+            track_number: 7,
+            type: 'track',
+            uri: 'spotify:track:6HmBWGDgxHNlE3AvX5aNN5',
+        },
+        {
+            album: {
+                album_type: 'album',
+                artists: [
+                    {
+                        external_urls: {
+                            spotify:
+                                'https://open.spotify.com/artist/1WgXqy2Dd70QQOU7Ay074N',
+                        },
+                        href:
+                            'https://api.spotify.com/v1/artists/1WgXqy2Dd70QQOU7Ay074N',
+                        id: '1WgXqy2Dd70QQOU7Ay074N',
+                        name: 'AURORA',
+                        type: 'artist',
+                        uri: 'spotify:artist:1WgXqy2Dd70QQOU7Ay074N',
+                    },
+                ],
+                external_urls: {
+                    spotify:
+                        'https://open.spotify.com/album/2iv4eCuGKJYsso1mDR48dt',
+                },
+                href:
+                    'https://api.spotify.com/v1/albums/2iv4eCuGKJYsso1mDR48dt',
+                id: '2iv4eCuGKJYsso1mDR48dt',
+                images: [
+                    {
+                        height: 640,
+                        url:
+                            'https://i.scdn.co/image/53a31a48a6db327175be725301ba758bb3bed354',
+                        width: 640,
+                    },
+                    {
+                        height: 300,
+                        url:
+                            'https://i.scdn.co/image/0c4210e5b5a44ed2dc6bb42260a4b97c0b0b46a9',
+                        width: 300,
+                    },
+                    {
+                        height: 64,
+                        url:
+                            'https://i.scdn.co/image/0e874917c51fe5ab7413f364b1ef1054920b9372',
+                        width: 64,
+                    },
+                ],
+                name: 'Infections Of A Different Kind (Step I)',
+                release_date: '2018-09-28',
+                release_date_precision: 'day',
+                total_tracks: 8,
+                type: 'album',
+                uri: 'spotify:album:2iv4eCuGKJYsso1mDR48dt',
+            },
+            artists: [
+                {
+                    external_urls: {
+                        spotify:
+                            'https://open.spotify.com/artist/1WgXqy2Dd70QQOU7Ay074N',
+                    },
+                    href:
+                        'https://api.spotify.com/v1/artists/1WgXqy2Dd70QQOU7Ay074N',
+                    id: '1WgXqy2Dd70QQOU7Ay074N',
+                    name: 'AURORA',
+                    type: 'artist',
+                    uri: 'spotify:artist:1WgXqy2Dd70QQOU7Ay074N',
+                },
+            ],
+            disc_number: 1,
+            duration_ms: 326918,
+            explicit: false,
+            external_ids: {
+                isrc: 'GBUM71804531',
+            },
+            external_urls: {
+                spotify:
+                    'https://open.spotify.com/track/1ppv75fAfo5i5vB3e7kp4l',
+            },
+            href: 'https://api.spotify.com/v1/tracks/1ppv75fAfo5i5vB3e7kp4l',
+            id: '1ppv75fAfo5i5vB3e7kp4l',
+            is_local: false,
+            is_playable: true,
+            name: 'Infections Of A Different Kind',
+            popularity: 47,
+            preview_url:
+                'https://p.scdn.co/mp3-preview/aa3327b064438deb444fb6bd21208ca9d7cec72d?cid=774b29d4f13844c495f206cafdad9c86',
+            track_number: 8,
+            type: 'track',
+            uri: 'spotify:track:1ppv75fAfo5i5vB3e7kp4l',
+        },
+    ],
+};
---
+++ test/track.test.ts
@@ -0,0 +1,51 @@
+import nock from 'nock';
+
+import { trackMock } from './mocks/track.mock';
+import { severalTracksMock } from './mocks/several-tracks.mock';
+import { checkMatchingTrackAttributes } from './common/matching-attributes.test';
+
+import { init, getTrack, getSeveralTracks } from '../src/lib';
+
+describe('Track requests', () => {
+    beforeEach(() => {
+        init('SomeToken');
+    });
+
+    describe('#getTrack()', () => {
+        const trackId = '6HmBWGDgxHNlE3AvX5aNN5';
+        const params = { market: 'BR' };
+
+        beforeEach(() => {
+            nock('https://api.spotify.com/v1')
+                .get(`/tracks/${trackId}`)
+                .query(params)
+                .reply(200, trackMock);
+        });
+
+        it('response should match all track attributes', async () => {
+            const trackResponse = await getTrack(trackId, params);
+            checkMatchingTrackAttributes(trackResponse, trackMock);
+        });
+    });
+
+    describe('#getSeveralTracks()', () => {
+        const tracks = ['6HmBWGDgxHNlE3AvX5aNN5', '1ppv75fAfo5i5vB3e7kp4l'];
+        const params = { ids: tracks.join(','), market: 'BR' };
+
+        beforeEach(() => {
+            nock('https://api.spotify.com/v1')
+                .get(`/tracks`)
+                .query(params)
+                .reply(200, severalTracksMock);
+        });
+
+        it('response should match all tracks attributes', async () => {
+            const tracksResponse = await getSeveralTracks(tracks, params);
+            for (let i = 0; i < tracksResponse.length; i++) {
+                const trackResponse = tracksResponse[i];
+                const trackMock = severalTracksMock.tracks[i];
+                checkMatchingTrackAttributes(trackResponse, trackMock);
+            }
+        });
+    });
+});
