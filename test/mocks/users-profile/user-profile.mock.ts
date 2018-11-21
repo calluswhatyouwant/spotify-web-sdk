c366d51b3fc70c579a2330d1fbe54021d8e9b9f7
JRobsonJr <jose.robson.junior@ccc.ufcg.edu.br>
2018-11-21 14:42:29 -0300

    Add mocks for the users profile requests

---
+++ test/mocks/users-profile/user-profile.mock.ts
@@ -0,0 +1,22 @@
+export const userProfileMock = {
+    display_name: 'José Renan',
+    external_urls: {
+        spotify: 'https://open.spotify.com/user/12180557353',
+    },
+    followers: {
+        href: null,
+        total: 80,
+    },
+    href: 'https://api.spotify.com/v1/users/12180557353',
+    id: '12180557353',
+    images: [
+        {
+            height: null,
+            url:
+                'https://scontent.xx.fbcdn.net/v/t1.0-1/p200x200/12239753_1009653019097615_636418336413030474_n.jpg?_nc_cat=100&_nc_ht=scontent.xx&oh=64380d96a6a7cdfa1ab5188b1c0e664f&oe=5C6A67FB',
+            width: null,
+        },
+    ],
+    type: 'user',
+    uri: 'spotify:user:12180557353',
+};
