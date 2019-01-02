1e30cdbf0558efaf2adb6e0d727b848ef656353f
José Renan <joserenansl99@gmail.com>
2019-01-02 00:02:19 -0300

    Edited start.ts script

--- src/start.ts
+++ src/start.ts
@@ -3,8 +3,6 @@ import * as spotify from './lib';
 const main = async () => {
     spotify.init({
         token: 'YOUR TOKEN HERE',
-        refreshToken: 'kikiki',
-        refreshTokenFunction: () => Promise.resolve('YOUR TOKEN THERE'),
     });
     const track = await spotify.getTrack('11dFghVXANMlKmJXsNCbNl');
     console.log(track);
