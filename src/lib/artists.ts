01f7a5550780fdb0bd88ca83f299ba6834d1013d
JRobsonJr <jrobsonjr16@gmail.com>
2019-02-20 15:50:17 -0300

    Update getArtistAlbums to use an optional object for optional params

--- src/lib/artists.ts
+++ src/lib/artists.ts
@@ -23,16 +23,25 @@ export const getSeveralArtists = async (ids: string[]): Promise<Artist[]> => {
 
 export const getArtistAlbums = async (
     id: string,
-    offset = 0,
-    limit = 20,
-    includeGroups?: string[],
-    market?: string
+    params?: {
+        offset?: number;
+        limit?: number;
+        includeGroups?: string[];
+        market?: string;
+    }
 ): Promise<Page<AlbumSimplified>> => {
-    const params: any = { params: { offset, limit, market } };
-    if (includeGroups) params.params.include_groups = includeGroups.join(',');
+    const config = {
+        params: {
+            ...params,
+            include_groups:
+                params && params.includeGroups
+                    ? params.includeGroups.join(',')
+                    : '',
+        },
+    };
     const response = await getAxiosSpotifyInstance().get(
         `/artists/${id}/albums`,
-        params
+        config
     );
     return new Page<AlbumSimplified>(response.data, AlbumSimplified);
 };
