a741830a0ba271a386b995ef8a82b5776ef83503
JRobsonJr <jrobsonjr16@gmail.com>
2019-02-27 12:00:08 -0300

    Implement library-related requests

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,6 +2,7 @@ export * from './albums';
 export * from './artists';
 export * from './driver';
 export * from './follow';
+export * from './library';
 export * from './personalization';
 export * from './playlists';
 export * from './player';
