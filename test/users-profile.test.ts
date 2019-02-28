126adc984900d3e4c214deb7552db3f1aa1f67ac
JRobsonJr <jrobsonjr16@gmail.com>
2019-02-27 22:58:12 -0300

    Implement users-profile tests

---
+++ test/users-profile.test.ts
@@ -0,0 +1,48 @@
+import nock from 'nock';
+
+import { userProfileMock } from './mocks/users-profile/user-profile.mock';
+import { currentUserProfileMock } from './mocks/users-profile/current-user-profile.mock';
+import {
+    checkMatchingPrivateUserAttributes,
+    checkMatchingPublicUserAttributes,
+} from './common/matching-attributes.test';
+
+import { init, getCurrentUserProfile, getUserProfile } from './../src/lib';
+
+describe('Users profile requests', () => {
+    beforeEach(() => {
+        init({ token: 'SomeToken' });
+    });
+
+    describe('#getCurrentUserProfile()', () => {
+        beforeEach(() => {
+            nock('https://api.spotify.com/v1')
+                .get('/me')
+                .reply(200, currentUserProfileMock);
+        });
+
+        it('response should match all user attributes', async () => {
+            const currentUserProfileResponse = await getCurrentUserProfile();
+            checkMatchingPrivateUserAttributes(
+                currentUserProfileResponse,
+                currentUserProfileMock
+            );
+        });
+    });
+
+    describe('#getUserProfile()', () => {
+        beforeEach(() => {
+            nock('https://api.spotify.com/v1')
+                .get('/users/12180557353')
+                .reply(200, userProfileMock);
+        });
+
+        it('response should match all user attributes', async () => {
+            const userProfileResponse = await getUserProfile('12180557353');
+            checkMatchingPublicUserAttributes(
+                userProfileResponse,
+                userProfileMock
+            );
+        });
+    });
+});
