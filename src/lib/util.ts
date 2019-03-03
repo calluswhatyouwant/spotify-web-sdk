eb8216d1b1ad044c81325df71362400e47c04259
JRobsonJr <jrobsonjr16@gmail.com>
2019-03-03 13:24:22 -0300

    Update propertiesToSnakeCase to support cases of body params in PUT requests

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,11 +1,12 @@
 import _ from 'lodash';
 
-export const propertiesToSnakeCase = (object: any) => {
-    const updatedObject = object;
+export const propertiesToSnakeCase = (object: any, bodyParams?: boolean) => {
+    const updatedObject: any = {};
     const keys = _.keys(object);
     keys.forEach((key: any) => {
         const value = object[key];
-        if (_.isArray(value)) updatedObject[_.snakeCase(key)] = value.join();
+        if (_.isArray(value) && !bodyParams)
+            updatedObject[_.snakeCase(key)] = value.join();
         else updatedObject[_.snakeCase(key)] = value;
     });
     return updatedObject;
