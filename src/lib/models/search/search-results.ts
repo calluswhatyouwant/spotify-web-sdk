7aee7841618c24d3951df529d7e5c5943a6f29fb
Robson Junior <jose.robson@splab.ufcg.edu.br>
2020-03-27 22:50:37 -0300

    Adapt SearchResults to allow episodes and shows

--- src/lib/models/search/search-results.ts
+++ src/lib/models/search/search-results.ts
@@ -1,7 +1,9 @@
 import Page from '../paging/page';
 import AlbumSimplified from '../album/album-simplified';
 import Artist from '../artist/artist';
+import EpisodeSimplified from '../episode/episode-simplified';
 import PlaylistSimplified from '../playlist/playlist-simplified';
+import ShowSimplified from '../show/show-simplified';
 import Track from '../track/track';
 
 class SearchResults {
@@ -9,6 +11,8 @@ class SearchResults {
     artists?: Page<Artist>;
     playlists?: Page<PlaylistSimplified>;
     tracks?: Page<Track>;
+    episodes?: Page<EpisodeSimplified>;
+    shows?: Page<ShowSimplified>;
 
     constructor(json: any) {
         if (json.albums) {
@@ -30,6 +34,20 @@ class SearchResults {
         }
         if (json.tracks) {
             this.tracks = new Page<Track>(json, Track, 'tracks');
+        }
+        if (json.episodes) {
+            this.episodes = new Page<EpisodeSimplified>(
+                json,
+                EpisodeSimplified,
+                'episodes'
+            );
+        }
+        if (json.shows) {
+            this.shows = new Page<ShowSimplified>(
+                json,
+                ShowSimplified,
+                'shows'
+            );
         }
     }
 }
