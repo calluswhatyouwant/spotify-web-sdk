cee3f4ba40e9deff031d9e6ae0bccb967cacf31f
klaywert <klaywert.souza@ccc.ufcg.edu.br>
2020-09-30 03:27:55 -0300

    Refactoring packages between docusaurus files and the project ones

--- docs/doc1.md
+++ docs/doc1.md
@@ -1,22 +1,22 @@
 ---
 id: doc1
-title: Latin-ish
-sidebar_label: Example Page
+title: Home
+sidebar_label: Nosso projeto
 ---
 
 Check the [documentation](https://docusaurus.io) for how to use Docusaurus.
 
-## Lorem
+## Inicio
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum dignissim ultricies. Fusce rhoncus ipsum tempor eros aliquam consequat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus elementum massa eget nulla aliquet sagittis. Proin odio tortor, vulputate ut odio in, ultrices ultricies augue. Cras ornare ultrices lorem malesuada iaculis. Etiam sit amet libero tempor, pulvinar mauris sed, sollicitudin sapien.
 
-## Mauris In Code
+## Meio
 
 ```
 Mauris vestibulum ullamcorper nibh, ut semper purus pulvinar ut. Donec volutpat orci sit amet mauris malesuada, non pulvinar augue aliquam. Vestibulum ultricies at urna ut suscipit. Morbi iaculis, erat at imperdiet semper, ipsum nulla sodales erat, eget tincidunt justo dui quis justo. Pellentesque dictum bibendum diam at aliquet. Sed pulvinar, dolor quis finibus ornare, eros odio facilisis erat, eu rhoncus nunc dui sed ex. Nunc gravida dui massa, sed ornare arcu tincidunt sit amet. Maecenas efficitur sapien neque, a laoreet libero feugiat ut.
 ```
 
-## Nulla
+## Fim
 
 Nulla facilisi. Maecenas sodales nec purus eget posuere. Sed sapien quam, pretium a risus in, porttitor dapibus erat. Sed sit amet fringilla ipsum, eget iaculis augue. Integer sollicitudin tortor quis ultricies aliquam. Suspendisse fringilla nunc in tellus cursus, at placerat tellus scelerisque. Sed tempus elit a sollicitudin rhoncus. Nulla facilisi. Morbi nec dolor dolor. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras et aliquet lectus. Pellentesque sit amet eros nisi. Quisque ac sapien in sapien congue accumsan. Nullam in posuere ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin lacinia leo a nibh fringilla pharetra.
 
---
+++ spotify-sdk/test/common/matching-attributes.test.ts
@@ -0,0 +1,254 @@
+import snakeCase from 'lodash.snakecase';
+import { expect } from 'chai';
+
+import {
+    Album,
+    AlbumSimplified,
+    Artist,
+    Track,
+    TrackSimplified,
+    Page,
+    Playlist,
+    CurrentlyPlaying,
+    Context,
+    CursorBasedPage,
+    PrivateUser,
+    PublicUser,
+    ArtistSimplified,
+    Category,
+    RecommendationSeed,
+} from '../../../src/lib/models';
+
+export const checkMatchingArtistSimplifiedAttributes = (
+    response: ArtistSimplified,
+    mock: any
+) => {
+    const attributes = ['externalUrls', 'href', 'id', 'name', 'type', 'uri'];
+    checkMatchingObjectAttributes(response, mock, attributes);
+};
+
+export const checkMatchingAlbumAttributes = (response: Album, mock: any) => {
+    const attributes = [
+        'copyrights',
+        'externalIds',
+        'genres',
+        'label',
+        'popularity',
+        'totalTracks',
+    ];
+    checkMatchingObjectAttributes(response, mock, attributes);
+    checkMatchingAlbumSimplifiedAttributes(response, mock);
+};
+
+export const checkMatchingAlbumSimplifiedAttributes = (
+    response: AlbumSimplified,
+    mock: any
+) => {
+    const attributes = [
+        'albumType',
+        'availableMarkets',
+        'externalUrls',
+        'href',
+        'id',
+        'images',
+        'name',
+        'releaseDate',
+        'releaseDatePrecision',
+        'type',
+        'uri',
+    ];
+    checkMatchingObjectAttributes(response, mock, attributes);
+    checkMatchingArtistArrays(response.artists, mock.artists);
+};
+
+export const checkMatchingArtistAttributes = (response: Artist, mock: any) => {
+    const attributes = [
+        'externalUrls',
+        'followers',
+        'genres',
+        'href',
+        'id',
+        'images',
+        'name',
+        'popularity',
+        'type',
+        'uri',
+    ];
+    checkMatchingObjectAttributes(response, mock, attributes);
+};
+
+export const checkMatchingTrackAttributes = (response: Track, mock: any) => {
+    const attributes = [
+        'availableMarkets',
+        'discNumber',
+        'durationMs',
+        'explicit',
+        'externalIds',
+        'externalUrls',
+        'href',
+        'id',
+        'isPlayable',
+        'linkedFrom',
+        'restrictions',
+        'name',
+        'popularity',
+        'previewUrl',
+        'trackNumber',
+        'type',
+        'uri',
+        'isLocal',
+    ];
+    checkMatchingObjectAttributes(response, mock, attributes);
+    checkMatchingAlbumSimplifiedAttributes(response.album, mock.album);
+    checkMatchingArtistArrays(response.artists, mock.artists);
+};
+
+export const checkMatchingTrackSimplifiedAttributes = (
+    response: TrackSimplified,
+    mock: any
+) => {
+    const attributes = [
+        'availableMarkets',
+        'discNumber',
+        'durationMs',
+        'explicit',
+        'externalUrls',
+        'href',
+        'id',
+        'isPlayable',
+        'linkedFrom',
+        'restrictions',
+        'name',
+        'previewUrl',
+        'trackNumber',
+        'type',
+        'uri',
+        'isLocal',
+    ];
+    checkMatchingObjectAttributes(response, mock, attributes);
+    checkMatchingArtistArrays(response.artists, mock.artists);
+};
+
+export const checkMatchingPagingObjectAttributes = (
+    response: Page<any>,
+    mock: any
+) => {
+    const attributes = ['href', 'limit', 'offset', 'total'];
+    checkMatchingObjectAttributes(response, mock, attributes);
+    expect(response.items).to.have.lengthOf(mock.items.length);
+};
+
+export const checkMatchingCurrentlyPlayingAttributes = (
+    response: CurrentlyPlaying,
+    mock: any
+) => {
+    if (response.context) {
+        checkMatchingContextAttributes(response.context, mock.context);
+    }
+    if (response.item && response.item instanceof Track) {
+        checkMatchingTrackAttributes(response.item, mock.item);
+    }
+    const attributes = [
+        'currentlyPlayingType',
+        'isPlaying',
+        'progressMs',
+        'timestamp',
+    ];
+    checkMatchingObjectAttributes(response, mock, attributes);
+};
+
+export const checkMatchingContextAttributes = (
+    response: Context,
+    mock: any
+) => {
+    const attributes = ['externalUrls', 'href', 'type', 'uri'];
+    checkMatchingObjectAttributes(response, mock, attributes);
+};
+
+export const checkMatchingCursorBasedPageAttributes = (
+    response: CursorBasedPage<any>,
+    mock: any
+) => {
+    const attributes = ['cursors', 'href', 'limit', 'total'];
+    checkMatchingObjectAttributes(response, mock, attributes);
+    expect(response.items).to.have.lengthOf(mock.items.length);
+    expect(response.next).to.be.equal(mock.next.split('?')[1]);
+};
+
+export const checkMatchingPrivateUserAttributes = (
+    response: PrivateUser,
+    mock: any
+) => {
+    const attributes = ['birthdate', 'country', 'email', 'product'];
+    checkMatchingPublicUserAttributes(response, mock);
+    checkMatchingObjectAttributes(response, mock, attributes);
+};
+
+export const checkMatchingPublicUserAttributes = (
+    response: PublicUser,
+    mock: any
+) => {
+    const attributes = [
+        'displayName',
+        'externalUrls',
+        'followers',
+        'href',
+        'id',
+        'images',
+        'type',
+        'uri',
+    ];
+    checkMatchingObjectAttributes(response, mock, attributes);
+};
+
+const checkMatchingObjectAttributes = (
+    response: any,
+    mock: any,
+    attributes: string[]
+) => {
+    for (const responseKey of attributes) {
+        const mockKey = snakeCase(responseKey);
+        expect(response)
+            .to.have.property(responseKey)
+            .that.is.eql(mock[mockKey]);
+    }
+};
+
+const checkMatchingArtistArrays = (
+    responseArtists: ArtistSimplified[],
+    mockArtists: any[]
+) => {
+    responseArtists.forEach((responseArtist, index) =>
+        checkMatchingArtistSimplifiedAttributes(
+            responseArtist,
+            mockArtists[index]
+        )
+    );
+};
+
+export const checkMatchingCategoryAttributes = (
+    response: Category,
+    mock: any
+) => {
+    const attributes = ['href', 'icons', 'id', 'name'];
+    checkMatchingObjectAttributes(response, mock, attributes);
+};
+
+export const checkMatchingRecommendationSeedAttributes = (
+    response: RecommendationSeed,
+    mock: any
+) => {
+    const attributes = ['href', 'id', 'type'];
+    checkMatchingObjectAttributes(response, mock, attributes);
+    expect(response.initialPoolSize).to.be.equal(mock.initialPoolSize);
+    expect(response.afterFilteringSize).to.be.equal(mock.afterFilteringSize);
+    expect(response.afterRelinkingSize).to.be.equal(mock.afterRelinkingSize);
+};
+
+export const checkMatchingPlaylistAttributes = (
+    response: Category,
+    mock: any
+) => {
+    const attributes = ['href', 'icons', 'id', 'name'];
+    checkMatchingObjectAttributes(response, mock, attributes);
+};
